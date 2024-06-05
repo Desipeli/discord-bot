804462a0ed7e956aa4c5788d2a7b3e9232be8bfd
desipeli <mr.desipeli@gmail.com>
2024-06-05 18:44:19 +0300

    alert channels for upcoming drops

--- cogs/rust_twitch_cog.py
+++ cogs/rust_twitch_cog.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 import os
 import requests
@@ -16,8 +17,10 @@ class RustTwitchCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.alerting_channels = set()
+        self.load_channels_from_file()
+        self.onGoingCampaignAlerted = False
+        self.comingCampaignAlerted = False
         self.checkForDrops.start()
-        self.onGoingCampaign = False
 
     @commands.command(name="add_rust_twitch")
     async def add_rust_twitch(self, ctx: commands.Context):
@@ -46,17 +49,68 @@ class RustTwitchCog(commands.Cog):
         response = requests.get(URL)
         soup = BeautifulSoup(response.text, features="html.parser")
         campaign = soup.find_all(class_="campaign")
-        if self.onGoingCampaign:
-            if not campaign:
+        not_started = soup.find_all(class_="not-started")
+        if self.onGoingCampaignAlerted:
+            if not campaign or not_started:
                 logging.info("Rust Twitch drop campaign ended")
-                self.onGoingCampaign = False
+                self.onGoingCampaignAlerted = False
         else:
-            if campaign:
+            if campaign and not not_started:
+                self.comingCampaignAlerted = False
                 logging.info("Rust Twitch drop campaign started")
-                self.onGoingCampaign = True
                 for channel_id in self.alerting_channels:
                     channel = self.bot.get_channel(channel_id)
                     await channel.send(f"Twitch drops {URL}")
+                self.onGoingCampaignAlerted = True
+
+        if not self.comingCampaignAlerted and campaign and not_started:
+            try:
+                round_number = soup.find(class_="round-info-number").text
+                round_title = soup.find(class_="round-info-title").text
+
+                # Timestamps can be found from data-date-ids, format: randomstring-timestamp
+                date_ids = [
+                    int(span["data-date-id"].split("-")[1]) for span in soup.find_all("span", class_="date")]
+                start_date = datetime.datetime.fromtimestamp(
+                    date_ids[0])
+                start_date = start_date.strftime('%d-%m-%Y %H:%M')
+                end_date = datetime.datetime.fromtimestamp(date_ids[1])
+                end_date = end_date.strftime('%d-%m-%Y %H:%M')
+
+                video_sources = []
+                for video in soup.find_all('video'):
+                    for source in video.find_all('source'):
+                        video_sources.append(source['src'])
+
+                for channel_id in self.alerting_channels:
+                    channel = self.bot.get_channel(channel_id)
+                    await channel.send(f"""
+Next Rust Twitch drop event: {start_date} to {end_date}
+{round_number}
+{round_title}
+                                       """)
+
+                # Discord displays only 5 videos per message
+                video_links = ""
+                for i, video_link in enumerate(video_sources):
+                    i = i+1
+                    video_links += f"{video_link}\n"
+                    if i % 5 == 0:
+                        await channel.send(video_links)
+                        video_links = ""
+                await channel.send(video_links)
+
+                self.comingCampaignAlerted = True
+            except Exception as e:
+                logging.error(e)
+
+    def load_channels_from_file(self):
+        try:
+            with open(CHANNEL_LIST_FULL_PATH, "r") as channel_list:
+                for channel_id in channel_list:
+                    self.alerting_channels.add(int(channel_id))
+        except Exception as e:
+            logging.error(e)
 
     def update_channels_file(self):
         try:
