c47d0df5269b7c4c8debf045009cd82dea548c90
desipeli <mr.desipeli@gmail.com>
2024-06-05 16:59:32 +0300

    save rust twitch drop channels to data directory

--- cogs/rust_twitch_cog.py
+++ cogs/rust_twitch_cog.py
@@ -1,9 +1,13 @@
 import logging
+import os
 import requests
+from config import RUST_TWITCH_DATA
 from bs4 import BeautifulSoup
 from discord.ext import commands, tasks
 
 URL = "https://twitch.facepunch.com/"
+CHANNEL_LIST_FILE = "channels.txt"
+CHANNEL_LIST_FULL_PATH = os.path.join(RUST_TWITCH_DATA, CHANNEL_LIST_FILE)
 
 
 class RustTwitchCog(commands.Cog):
@@ -15,13 +19,29 @@ class RustTwitchCog(commands.Cog):
         self.checkForDrops.start()
         self.onGoingCampaign = False
 
-    @commands.command(name="add_rust_twich")
-    async def add_rust_twich(self, ctx: commands.Context):
+    @commands.command(name="add_rust_twitch")
+    async def add_rust_twitch(self, ctx: commands.Context):
         """ Alert this channel when new twitch drops are available """
         self.alerting_channels.add(ctx.channel.id)
+        try:
+            self.update_channels_file()
+        except Exception:
+            await ctx.send("Could not permanently update channel list")
+            return
         await ctx.send(f"Alerting")
 
-    @tasks.loop(minutes=1)
+    @commands.command(name="remove_rust_twitch")
+    async def remove_rust_twitch(self, ctx: commands.Context):
+        """ Remove from the list channels"""
+        self.alerting_channels.remove(ctx.channel.id)
+        try:
+            self.update_channels_file()
+        except Exception:
+            await ctx.send("Could not permanently update channel list")
+            return
+        await ctx.send("Not alerting")
+
+    @tasks.loop(minutes=77)
     async def checkForDrops(self):
         response = requests.get(URL)
         soup = BeautifulSoup(response.text, features="html.parser")
@@ -37,3 +57,12 @@ class RustTwitchCog(commands.Cog):
                 for channel_id in self.alerting_channels:
                     channel = self.bot.get_channel(channel_id)
                     await channel.send(f"Twitch drops {URL}")
+
+    def update_channels_file(self):
+        try:
+            with open(CHANNEL_LIST_FULL_PATH, "w") as channel_list:
+                for channel_id in self.alerting_channels:
+                    channel_list.write(f"{channel_id}\n")
+        except Exception as e:
+            logging.error(e)
+            raise Exception("Could not update channels")
---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Cog data dirs
+
+RUST_TWITCH_DATA = os.path.join(ROOT_DIR, "data/rust_twitch_drops")
