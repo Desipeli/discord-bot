7e1426d60ecd4402f9f44c05371432b890375eb4
desipeli <mr.desipeli@gmail.com>
2024-06-05 17:00:47 +0300

    data dir for gpt cog

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 # Cog data dirs
 
 RUST_TWITCH_DATA = os.path.join(ROOT_DIR, "data/rust_twitch_drops")
+GPT_DATA = os.path.join(ROOT_DIR, "data/gpt")
